b8096ff1f88f6fd21cd8a750f075484c74dc7819
xaviergg_esp <xavier@erpsuccesspartners.com>
2023-09-11 23:19:31 +0800

    update dist repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/middlemarch-vite-test/",
   plugins: [
     vue(),
     nightwatchPlugin()
